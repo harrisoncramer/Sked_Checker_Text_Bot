4edafab002ff99885da731819b58a1b368a1a8eb
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2020-02-20 10:43:52 -0500

    added exit handler

--- mongodb/connect/index.js
+++ mongodb/connect/index.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const logger = require("../../logger")(module);
+const mongoose = require('mongoose');
+const logger = require('../../logger')(module);
 
 module.exports = async () => {
   let uri = 'mongodb://localhost:27017/sked_checker?authSource=admin';
-  
+
   let options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -11,8 +11,22 @@ module.exports = async () => {
     user: 'admin',
     pass: process.env.MONGO_PASS,
   };
-  
+
   let db = await mongoose.connect(uri, options);
-  logger.info("Connected to MongoDb.")
-  return db;  
+  logger.info('Connected to MongoDb.');
+
+  process.on('SIGINT', async () => {
+    logger.info('SIGINT SIGNAL RECIEVED');
+    try {
+      await db.disconnect();
+      logger.info('Closed DB Connection.');
+      process.exit(0);
+    } catch (err) {
+      logger.error('There was a problem closing the db', err);
+      process.exit(1);
+    }
+  });
+  
+  return db;
+
 };
--- setup/index.js
+++ setup/index.js
@@ -1,44 +1,43 @@
 const puppeteer = require('puppeteer-extra');
 const randomUser = require('random-useragent');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-const getProxy = require("./proxies");
+const getProxy = require('./proxies');
 
-const { asyncForEach, getRandom } = require('../util');
-const logger = require("../logger")(module);
+const {asyncForEach, getRandom} = require('../util');
+const logger = require('../logger')(module);
 
 module.exports = {
-  setupPuppeteer: async ({ type }) => {
-
+  setupPuppeteer: async ({type}) => {
     // Set initial variables
-    const isProduction = process.env.NODE_ENV === "production";
+    const isProduction = process.env.NODE_ENV === 'production';
     let proxy = null;
     const args = ['--no-sandbox'];
 
     // Determine tor/proxy for browser
-    if(type === 'tor'){
-      let ports = process.env.TOR_PORTS.split(" ");
+    if (type === 'tor') {
+      let ports = process.env.TOR_PORTS.split(' ');
       let portIndex = getRandom(0, ports.length - 1)();
       let port = isProduction ? ports[portIndex] : '9050';
       proxy = `socks5://127.0.0.1:` + port;
     }
-    if(type === 'proxy'){
+    if (type === 'proxy') {
       let proxyData = await getProxy();
       proxy = `http://${proxyData.ip}:${proxyData.port}`;
-    };
-    if(proxy){
+    }
+    if (proxy) {
       args.push(`--proxy-server=${proxy}`);
-    };
-  
+    }
+
     /// Initialize Browser
     const browser = await puppeteer.launch({
       headless: isProduction,
       devtools: !isProduction,
-      args
+      args,
     });
     browser.on('disconnected', () => {
-      logger.info("Browser was disconnected.");
+      logger.info('Browser was disconnected.');
     });
 
     ///// Page setup
@@ -52,88 +51,107 @@ module.exports = {
 
     //// Confirm page is working
     await page.goto('https://check.torproject.org/');
-    const isUsingTor = await page.$eval('body', el => el.innerHTML.includes('Congratulations. This browser is configured to use Tor'));
+    const isUsingTor = await page.$eval('body', el =>
+      el.innerHTML.includes(
+        'Congratulations. This browser is configured to use Tor',
+      ),
+    );
     if (!isUsingTor && type === 'tor') {
-      logger.error(`Browser is not using Tor. Exiting...`)
+      logger.error(`Browser is not using Tor. Exiting...`);
       return await browser.close();
     }
 
     return {browser, page};
   },
   setPageBlockers: async page => {
-     await page.setRequestInterception(true);
-     const blockedResources = [
-       'quantserve',
-       'adzerk',
-       'doubleclick',
-       'adition',
-       'exelator',
-       'sharethrough',
-       'twitter',
-       'google-analytics',
-       'fontawesome',
-       'facebook',
-       'analytics',
-       'optimizely',
-       'clicktale',
-       'mixpanel',
-       'zedo',
-       'clicksor',
-       'tiqcdn',
-       'googlesyndication',
-       'youtube',
-     ];
+    await page.setRequestInterception(true);
+    const blockedResources = [
+      'quantserve',
+      'adzerk',
+      'doubleclick',
+      'adition',
+      'exelator',
+      'sharethrough',
+      'twitter',
+      'google-analytics',
+      'fontawesome',
+      'facebook',
+      'analytics',
+      'optimizely',
+      'clicktale',
+      'mixpanel',
+      'zedo',
+      'clicksor',
+      'tiqcdn',
+      'googlesyndication',
+      'youtube',
+    ];
 
-     page.on('request', async request => {
-       const url = request.url().toLowerCase();
-       // const headers = request.headers();
-       if (url.endsWith('.mp4') || 
-           url.endsWith('.avi') || 
-           url.endsWith('.flv') || 
-           url.endsWith('.mov') || 
-           url.endsWith('.wmv') || 
-           ['image', 'stylesheet', 'media', 'jpg', 'png'].includes(request.resourceType()) ||
-           blockedResources.some(resource => url.indexOf(resource) !== -1)
-           ) {
-             try {
-               await request.abort();
-             } catch (err) {
-               if(err.message !== "Request is already handled!"){
-                 logger.info(`Problem blocking resource from ${url}`);
-               }
-             }
-         } else {
-           try {
-             await request.continue();
-           } catch (err) {
-             if(err.message !== "Request is already handled!"){
-               logger.info(`Problem blocking resource from ${url}`);
-             }
-           }
-         }
-     });
-  },
-  setPageScripts: async page => {
-    await page.addScriptTag({ path: "./setup/functions/index.js" });
-    await page.addScriptTag({ path: "./setup/jquery/jquery-3.4.1.slim.min.js" }); /// "https://code.jquery.com/jquery-3.4.1.slim.min.js" }); // Add jQuery...
-  },
-  launchPuppeteerBots: async ({ page, browser, bot, instances, db }) => {
-    await asyncForEach(instances, async args => {
-      try {
-        await bot({ page, browser, db, args });
-      } catch (err) {
-        let deadPages = await browser.pages();
-        await Promise.all(deadPages.filter((x,i) => i !== 0).map(page => page.close()));
-        logger.error(`${args.schema.collection.collectionName} > There was a problem with the bot: `, err);
+    page.on('request', async request => {
+      const url = request.url().toLowerCase();
+      // const headers = request.headers();
+      if (
+        url.endsWith('.mp4') ||
+        url.endsWith('.avi') ||
+        url.endsWith('.flv') ||
+        url.endsWith('.mov') ||
+        url.endsWith('.wmv') ||
+        ['image', 'stylesheet', 'media', 'jpg', 'png'].includes(
+          request.resourceType(),
+        ) ||
+        blockedResources.some(resource => url.indexOf(resource) !== -1)
+      ) {
+        try {
+          await request.abort();
+        } catch (err) {
+          if (err.message !== 'Request is already handled!') {
+            logger.info(`Problem blocking resource from ${url}`);
+          }
+        }
+      } else {
+        try {
+          await request.continue();
+        } catch (err) {
+          if (err.message !== 'Request is already handled!') {
+            logger.info(`Problem blocking resource from ${url}`);
+          }
+        }
       }
     });
   },
-  launchAxiosBots: async ({ db, proxyData, bot, instances }) => {
+  setPageScripts: async page => {
+    await page.addScriptTag({path: './setup/functions/index.js'});
+    await page.addScriptTag({path: './setup/jquery/jquery-3.4.1.slim.min.js'}); /// "https://code.jquery.com/jquery-3.4.1.slim.min.js" }); // Add jQuery...
+  },
+  launchPuppeteerBots: async ({page, browser, bot, instances, db}) => {
     await asyncForEach(instances, async args => {
       try {
-        await bot({ db, proxyData, args });
+        await bot({page, browser, db, args});
       } catch (err) {
-        logger.error(`${args.schema.collection.collectionName} > There was a problem with the bot: `, err);
+        let deadPages = await browser.pages();
+        await Promise.all(
+          deadPages.filter((x, i) => i !== 0).map(page => page.close()),
+        );
+        logger.error(
+          `${
+            args.schema.collection.collectionName
+          } > There was a problem with the bot: `,
+          err,
+        );
+      }
+    });
+  },
+  launchAxiosBots: async ({db, proxyData, bot, instances}) => {
+    await asyncForEach(instances, async args => {
+      try {
+        await bot({db, proxyData, args});
+      } catch (err) {
+        logger.error(
+          `${
+            args.schema.collection.collectionName
+          } > There was a problem with the bot: `,
+          err,
+        );
       }
     });
   },
